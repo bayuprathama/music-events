52d4488cd2412d4b79b2566c98b5cd40831867ae
Bayu Prathama <bayemprathama@gmail.com>
2021-11-24 21:25:26 +0800

    add 404 and showcase

---
+++ components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Showcase.module.css'
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome To The Pittt!!</h1>
+      <h1>Find the best concert in your area!</h1>
+    </div>
+  )
+}
