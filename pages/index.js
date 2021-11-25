b8e4212fe7077fc297d1878b629b0f70be3bcb83
Bayu Prathama <dev.bayuprathama@gmail.com>
2021-11-25 20:58:58 +0800

    add API ROUTE

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
-import Layout from '@/components/Layout'
+import Layout from '@/components/Layout';
+import { useEffect } from 'react';
+import { API_URL } from '@/config/index';
 
-export default function Home() {
+export default function Home({ events }) {
+  console.log(API_URL);
+  console.log(events);
   return (
     <Layout>
       <h1>Home page</h1>
     </Layout>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+  };
 }
